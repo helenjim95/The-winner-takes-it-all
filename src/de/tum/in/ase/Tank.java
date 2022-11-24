c2172802053d679a45c1e5e3d7b8a89a197e77f5
ge32goy <hanyin.chen@tum.de>
2022-11-24 23:37:27 +0100

    commit

--- src/de/tum/in/ase/Tank.java
+++ src/de/tum/in/ase/Tank.java
@@ -31,6 +31,10 @@ public class Tank extends Player {
     return shield;
   }
 
+  public Armor equipShield(Armor shield) {
+      this.shield = shield;
+  }
+
   @Override
     public void attack(Player target) {
       if (!this.getWeapon().isEquipped()) {
