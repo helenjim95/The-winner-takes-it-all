79621565938d0055d5428d95fef9970119b79078
ge32goy <hanyin.chen@tum.de>
2022-11-29 21:03:33 +0100

    commit

--- src/de/tum/in/ase/Tank.java
+++ src/de/tum/in/ase/Tank.java
@@ -73,7 +73,7 @@ public class Tank extends Player {
 //      when a tank uses his ability, the armor is increased and afterwards he deals with the enemy attack.
         int length = this.abilities.size() - 1;
         if (length == 0) {
-        System.out.printf("%s has no skills to use!%n", this.name);
+            System.out.printf("%s has no skills to use!%n", this.name);
         } else {
             int randomIndex = (int) (Math.random() * length);
             Ability ability = abilities.get(randomIndex);
@@ -86,4 +86,92 @@ public class Tank extends Player {
             }
         }
     }
+
+    public void equipItemsHelperMethod (Item item){
+        if (item.getClass().equals(Weapon.class)) {
+            this.weapon = (Weapon) item;
+            this.strength += item.getStrength();
+            this.intelligence += item.getIntelligence();
+            this.agility += item.getAgility();
+            this.spirit += item.getSpirit();
+        } else if (item.getClass().equals(Armor.class)) {
+            this.setAmountOfArmor(this.getAmountOfArmor() + ((Armor) item).getAmountOfArmor());
+            this.strength += item.getStrength();
+            this.intelligence += item.getIntelligence();
+            this.agility += item.getAgility();
+            this.spirit += item.getSpirit();
+        } else {
+            System.out.println("This item is neither weapon or armor");
+        }
+    }
+
+
+    //  Done: The equipItems method uses the available weapon and armor list.
+    //   Armor should be equipped according to the type (Helmet, Chest, Hands, Legs, Boots)
+    //   and for each type, only the first item should be equipped.
+    //   After equipping, any item player's attributes should be changed accordingly.
+    //   Same applies to equipping the weapon.
+protected void equipItems() {
+    //      Done: set Item.equipped to true if in list / same for weapon (equip only the first item of the same type)
+//      Armor should be equipped according to the type (Helmet, Chest, Hands, Legs, Boots)
+    if (!armor.isEmpty()) {
+        for (int i = 0; i < armor.size(); i++) {
+            Armor arm = armor.get(i);
+            if (arm.getSpecification().equals(this.specification)) {
+                switch (arm.getType()) {
+                    case "Helmet":
+                        if (this.helmet == null || !this.helmet.isEquipped()) {
+//               After equipping, any item player's attributes should be changed accordingly.
+                            this.helmet = arm;
+                            equipItemsHelperMethod(arm);
+                            this.helmet.setEquipped(true);
+                            System.out.println(name + "is equipped with " + getHelmet().getType());
+                        }
+                        break;
+                    case "Chest":
+                        if (this.chest == null || !this.chest.isEquipped()) {
+                            this.chest = arm;
+                            equipItemsHelperMethod(arm);
+                            this.chest.setEquipped(true);
+                            System.out.println(name + "is equipped with " + getChest().getType());
+                        }
+                        break;
+                    case "Hands":
+                        if (this.hands == null || !this.hands.isEquipped()) {
+                            this.hands = arm;
+                            equipItemsHelperMethod(arm);
+                            this.hands.setEquipped(true);
+                            System.out.println(name + "is equipped with " + getHands().getType());
+                        }
+                        break;
+                    case "Legs":
+                        if (this.legs == null || !this.legs.isEquipped()) {
+                            this.legs = arm;
+                            equipItemsHelperMethod(arm);
+                            this.legs.setEquipped(true);
+                            System.out.println(name + "is equipped with " + getLegs().getType());
+                        }
+                        break;
+                    case "Boots":
+                        if (this.boots == null || !this.boots.isEquipped()) {
+                            this.boots = arm;
+                            equipItemsHelperMethod(arm);
+                            this.boots.setEquipped(true);
+                            System.out.println(name + "is equipped with " + getBoots().getType());
+                        }
+                        break;
+                    default:
+                        System.out.printf("There is no armor with the type of %s%n", arm.getType());
+                        break;
+                }
+            }
+        }
+    }
+    if (this.weapon == null || !this.weapon.isEquipped()) {
+        if (this.specification.equals(this.weapon.getSpecification()) && this.weaponType.equals(this.weapon.getType())) {
+            equipItemsHelperMethod(weapon);
+            this.weapon.setEquipped(true);
+        }
+    }
+}
 }
